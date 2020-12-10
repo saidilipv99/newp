7f8928fbadca620a8f70b8d59ba2ea7e6d331f21
saidilip <sai.dilip.v99@gmail.com>
2020-12-10 21:43:32 +0530

    update the d integer

---
+++ src/Javaclass1.java
@@ -0,0 +1,10 @@
+public class Javaclass1 {
+    public static  void main(String arg[]){
+        int a,b,c,d;
+        a=10;
+        b=20;
+        d=20;
+        c=a+b-d;
+        System.out.println(c);
+    }
+}
